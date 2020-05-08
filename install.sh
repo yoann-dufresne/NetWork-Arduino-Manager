f68305c364c277ccd32d2f0b0d5809f5adeded4b
Yoann Dufresne <yoann.dufresne0@gmail.com>
2020-05-08 09:57:42 +0200

    structural commit

---
+++ install.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+pip3 install --user -e .
+curl -fsSL https://raw.githubusercontent.com/arduino/arduino-cli/master/install.sh | sh
---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+
+setup(
+    name='Network Ardiono Manager',
+    version='0.1dev',
+    packages=[],
+    license='AGPL V3',
+    long_description=open('README.md').read(),
+)
